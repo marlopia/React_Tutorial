2b427d4ad35cc157e9f1d6403cd23eddb35cb494
Mario Lopez Gonzalez <marlopiacj@gmail.com>
2026-01-15 17:02:21 +0100

    Added scroll animation and cleaned up css

--- react-tutorial/src/App.jsx
+++ react-tutorial/src/App.jsx
@@ -1,42 +1,61 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useRef } from "react";
 import Navbar from "./Navbar.jsx";
 import Product from "./Product.jsx";
 import Footer from "./Footer.jsx";
 import socials from "./data/socials.json";
+import products from "./data/products.json";
 import "./App.css";
 
 function App() {
+  const gridRef = useRef(null);
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  const onMouseDown = (e) => {
+    isDown = true;
+    startX = e.pageX - gridRef.current.offsetLeft;
+    scrollLeft = gridRef.current.scrollLeft;
+  };
+
+  const onMouseMove = (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - gridRef.current.offsetLeft;
+    const walk = x - startX;
+    gridRef.current.scrollLeft = scrollLeft - walk;
+  };
+
+  const onMouseUpOrLeave = () => {
+    isDown = false;
+  };
+
   return (
     <>
       <Navbar
         title="SuperMarket Testing"
         text="Get a fruit and a fresh React-ion!"
       />
-      <div className="product-grid">
-        <Product
-          imgLink="https://res.cloudinary.com/dbfn5lnvx/image/upload/v1726640668/react-tutorial/superm-v2/pineapple.jpg"
-          name="Pineapple"
-          price={2.0}
-        />
-        <Product
-          imgLink="https://img.freepik.com/free-psd/close-up-delicious-apple_23-2151868338.jpg"
-          name="Apple"
-          price={3.15}
-        />
-        <Product
-          imgLink="https://images.unsplash.com/photo-1587132137056-bfbf0166836e?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmFuYW5hfGVufDB8fDB8fHww"
-          name="Banana"
-          price={1.5}
-        />
-        <Product
-          imgLink="https://static.vecteezy.com/system/resources/thumbnails/005/170/812/small/pear-green-with-pear-slices-and-leaves-vitamins-healthy-food-fruit-on-a-white-background-realistic-3d-illustration-vector.jpg"
-          name="Pear"
-          price={1.66}
-        />
+
+      <div
+        className="product-grid"
+        ref={gridRef}
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUpOrLeave}
+        onMouseLeave={onMouseUpOrLeave}
+      >
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            imgLink={product.imgLink}
+            name={product.name}
+            price={product.price}
+          />
+        ))}
       </div>
-      < Footer links={socials} />
+
+      <Footer links={socials} />
     </>
   );
 }
